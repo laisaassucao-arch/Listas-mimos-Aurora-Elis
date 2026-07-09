bf3ad18768238e720fa57652ecfe8b053b591443
laisaassucao-arch <laisaassucao@gmail.com>
2026-07-09 19:54:01 -0300

    Update presentes.js

--- presentes.js
+++ presentes.js
@@ -1,1 +1,72 @@
-
+export const presentes = [
+  {
+    id: "baba-eletronica",
+    categoria: "quarto",
+    nome: "Babá Eletrônica",
+    link: "https://s.shopee.com.br/AKYqseebwL",
+    reservado: false
+  },
+  {
+    id: "ruido-branco",
+    categoria: "quarto",
+    nome: "Ruído Branco",
+    link: "https://s.shopee.com.br/5fn1JrSDxR",
+    reservado: false
+  },
+  {
+    id: "carrinho",
+    categoria: "passeio",
+    nome: "Carrinho Moisés + Bebê Conforto",
+    link: "https://s.shopee.com.br/8pk35OJB4V",
+    reservado: false
+  },
+  {
+    id: "lixeira",
+    categoria: "quarto",
+    nome: "Lixeira Antiodor",
+    link: "https://s.shopee.com.br/8V7ChaiFJg",
+    reservado: false
+  },
+  {
+    id: "colmeia",
+    categoria: "organizacao",
+    nome: "Kit Colmeia Organizadora",
+    link: "https://s.shopee.com.br/18eaRtgf8",
+    reservado: false
+  },
+  {
+    id: "cabides",
+    categoria: "organizacao",
+    nome: "Organizador de Cabides",
+    link: "https://s.shopee.com.br/5LAAwPDoWP",
+    reservado: false
+  },
+  {
+    id: "kit-higiene",
+    categoria: "higiene",
+    nome: "Kit Higiene",
+    link: "https://s.shopee.com.br/2BD9AqsXEw",
+    reservado: false
+  },
+  {
+    id: "cesto",
+    categoria: "organizacao",
+    nome: "Cesto Organizador",
+    link: "https://s.shopee.com.br/18ebPVRzE",
+    reservado: false
+  },
+  {
+    id: "body-curta",
+    categoria: "roupinhas",
+    nome: "Body Manga Curta",
+    link: "https://s.shopee.com.br/9AMtU8DZp2",
+    reservado: false
+  },
+  {
+    id: "kit-body",
+    categoria: "roupinhas",
+    nome: "Kit Body",
+    link: "https://s.shopee.com.br/6ffYVbvmAk",
+    reservado: false
+  }
+];
