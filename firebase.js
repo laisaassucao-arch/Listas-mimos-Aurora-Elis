03ef3a1c246e328278d8e1541576cb964b8c74c7
laisaassucao-arch <laisaassucao@gmail.com>
2026-07-09 21:05:11 -0300

    Finaliza lista de mimos

--- firebase.js
+++ firebase.js
@@ -4,12 +4,12 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCvAXVqH0L0lnzb20p3RIvosjx_in6Uw0b0",
-  authDomain: "mimos-da-aurora.firebaseapp.com",
-  projectId: "mimos-da-aurora",
-  storageBucket: "mimos-da-aurora.firebasestorage.app",
-  messagingSenderId: "1056970312986",
-  appId: "1:1056970312986:web:7e09482e2b4770f4b5b0a3"
+  apiKey: "AIzaSyCQ057Zbh3rNOseMJO9NV0rAvit0uMVmX0",
+  authDomain: "lista-aurora-elis.firebaseapp.com",
+  projectId: "lista-aurora-elis",
+  storageBucket: "lista-aurora-elis.firebasestorage.app",
+  messagingSenderId: "921516765933",
+  appId: "1:921516765933:web:75103d68b6a185dd2092e4"
 };
 
 const app = initializeApp(firebaseConfig);
